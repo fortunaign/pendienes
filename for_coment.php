f8775e33abc2d5e05ba8e08fcffa4c2d891ef547
fortunaign <43297312+fortunaign@users.noreply.github.com>
2020-09-02 15:51:29 -0400

    Formularios

---
+++ for_coment.php
@@ -0,0 +1,10 @@
+<h2>Comentario</h2>
+<label for="">Pendiente</label>
+<br>
+<input type="text" readonly>
+<br>
+<label for="">Comentario</label>
+<br>
+<textarea name="" id="" cols="30" rows="10"></textarea>
+<br>
+<input type="submit" value="Guardar">
---
+++ for_input.php
@@ -0,0 +1,14 @@
+<h2>Nuevo</h2>
+<label for="">Area</label>
+<br>
+<input type="text">
+<br>
+<label for="">Titulo</label>
+<br>
+<input type="text">
+<br>
+<label for="">Detalles</label>
+<br>
+<textarea name="" id="" cols="30" rows="10"></textarea>
+<br>
+<input type="submit" value="Guardar">
