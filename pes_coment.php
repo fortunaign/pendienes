7cd4df5e7e4b914362fa4a77ec88cf301e79d842
fortunaign <43297312+fortunaign@users.noreply.github.com>
2020-09-29 16:36:45 -0300

    detalles

--- pes_coment.php
+++ pes_coment.php
@@ -11,13 +11,10 @@ if($result->num_rows > 0){
 ?>
 <div class="row">
     <div class="col-12 my-4">
-    <form action="database\pes_process.php" method="post" class="my-3"><input type="hidden" name="pes_id" value="<?php echo $id; ?>"><input type="submit" value="Finalizar" name="pes_cerrar" class="btn btn-danger"></form>
+        <form action="database\pes_process.php" method="post" class="my-3"><input type="hidden" name="pes_id" value="<?php echo $id; ?>"><input type="submit" value="Finalizar" name="pes_cerrar" class="btn btn-danger"></form>
         <h1><?php echo $row['pes_titulo']; ?></h1>
-        <p>
-            <?php echo $row['pes_detalles']; ?>
-            <br>
-            <?php echo $row['pes_fecha']; ?>
-        </p>
+        <p><?php echo $row['pes_detalles']; ?></p>
+        <p><?php echo $row['pes_fecha']." | ".$row['pes_hora']; ?></p>
     </div>
 <?php
         }
@@ -33,26 +30,25 @@ if($result->num_rows > 0){
             <input type="submit" value="Guardar" name="pes_coment" class="btn btn-primary">
         </form>
     </div>
+
     <div class="col-sm-6">
-        <div class="row">
-            <h4 class="my-3">Notas</h4>     
-            <?php
-                $sql="SELECT * FROM pes_coment WHERE pes_id=".$_GET['id'];
-                $result=$conn->query($sql);
-                if($result->num_rows > 0){
-                    while($row = $result->fetch_assoc()){
-                ?>
-                <div class="col-12 alert alert-info">
-                    <?php echo $row['pes_coment']; ?>
-                    <br>
-                    <strong><?php echo $row['pes_fecha']; ?></strong>
-                </div>
-                <?php
-                    }
-                }
-                $conn->close();
+        <h4 class="my-3">Notas</h4>     
+        <?php
+            $sql="SELECT * FROM pes_coment WHERE pes_id=".$_GET['id'];
+            $result=$conn->query($sql);
+            if($result->num_rows > 0){
+                while($row = $result->fetch_assoc()){
             ?>
-        </div>
+            <div class="col-12 alert alert-info">
+                <?php echo $row['pes_coment']; ?>
+                <br>
+                <strong><?php echo $row['pes_fecha']." | ".$row['pes_hora']; ?></strong>
+            </div>
+            <?php
+                }
+            }
+            $conn->close();
+        ?>
     </div>
 </div>
 <?php require 'pes_footer.php'; ?>
--- pes_lista.php
+++ pes_lista.php
@@ -1,43 +1,42 @@
 <div class="row">
-    <div class="col-lg-12">
-        <h1 class="display-1 my-4">
+    <div class="col-12">
+        <h1 class="display-3 my-4">
             Apuntes
         </h1>
         <a href="pes_input.php" class="">
             <button type="button" class="btn btn-primary bt-lg my-4">
-                + Nuevo
+                <strong>+</strong> Nuevo
             </button>
         </a>
     </div>
-</div>
+    
+    <?php
+    require 'database\conn.php';
+    $sql="SELECT * FROM pes_general WHERE pes_activo=0";
+    $result=$conn->query($sql);
+    if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()){
+    ?>
 
-<div class="row">
-<?php
-require 'database\conn.php';
-$sql="SELECT * FROM pes_general WHERE pes_activo=0";
-$result=$conn->query($sql);
-if($result->num_rows > 0){
-    while($row = $result->fetch_assoc()){
-?>
-
-    <div class="col-sm-12 col-md-4 col-lg-4">
+    <div class="col-12 col-md-4 col-lg-4">
         <p>
             <strong><?php echo $row['pes_titulo'];?></strong>
             <br>
-            <a href="pes_coment.php?id=<?php echo $row['pes_id'];?>">Leer mas...</a></BR>
+            <a href="pes_coment.php?id=<?php echo $row['pes_id'];?>">Leer mas..</a>
         </p>
-        
     </div>
-    <div class="col-sm-12 col-md-8 col-lg-8">
+
+    <div class="col-12 col-md-8 col-lg-8">
         <p class="text-muted">
-            <?php echo $row['pes_fecha'];?>
+            <?php echo $row['pes_fecha']." | ".$row['pes_hora'];?>
             <br>
             <?php echo $row['pes_area'];?>
         </p>
-    </div>    
-<?php
+    </div>
+
+    <?php
+        }
     }
-}
-$conn->close();
-?>
+    $conn->close();
+    ?>
 </div>
